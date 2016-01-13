f93448862103978465ca5b71f615df952bf80a70
majian <majian159@live.com>
2016-01-13 11:38:19 +0800

    遗漏的提交。

---
+++ OrleansSamples/Clusters/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Orleans;
+using Sample.Interfaces;
+using System;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GrainClient.Initialize();
+
+            while (true)
+            {
+                Console.WriteLine("请输入用户的手机号码来判断是否存在：");
+                var mobileNumber = Console.ReadLine();
+                var userService = GrainClient.GrainFactory.GetGrain<IUserGrain>(mobileNumber);
+                Console.WriteLine($"用户{mobileNumber}，{(userService.Exist().Result ? "已经存在" : "还不存在")}");
+                if (mobileNumber == "exit")
+                    GrainClient.Uninitialize();
+            }
+        }
+    }
+}
